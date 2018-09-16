1da691bb2ea02995066c3f7049e50d9e2d3de2da
mjmeilahn <mjmeilahn@gmail.com>
2018-09-16 14:51:55 -0700

    Adding PLUS MINUS PHP

--- 3-plus-minus/index.php
+++ 3-plus-minus/index.php
@@ -1,7 +1,38 @@
 <?php
 
-// INPUT $arr: [26, 0, -3, -85, 73, 43]
-// EXPECTED OUTPUT: [0.5, 0.33, 0.17]
+// INPUT $numbers: [26, 0, -3, -85, 73, 43]
+// EXPECTED OUTPUT: [0.500000, 0.333333, 0.166667]
+
+$numbers = [26, 0, -3, -85, 73, 43];
+
+function plusMinus($arr) {
+    $denom = count($arr);
+    $positives = 0;
+    $negatives = 0;
+    $zeroes = 0;
+
+    foreach ($arr as $i) {
+        if ($i === 0) {
+            $zeroes++;
+        } else if ($i > 0) {
+            $positives++;
+        } else if ($i < 0) {
+            $negatives++;
+        }
+    }
+
+    $positives = $positives / $denom;
+    $negatives = $negatives / $denom;
+    $zeroes = $zeroes / $denom;
+
+    $positivePercent = number_format($positives, 6, '.', '');
+    $negativePercent = number_format($negatives, 6, '.', '');
+    $zeroPercent = number_format($zeroes, 6, '.', '');
+
+    echo "$positivePercent \n$negativePercent \n$zeroPercent";
+}
 
 
+
+echo plusMinus($numbers);
 
